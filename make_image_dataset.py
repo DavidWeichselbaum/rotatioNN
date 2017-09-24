5caefc477a77098716718b84c6cc757cbdfcb3f5
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2017-09-24 15:52:26 +0200

    Fix range in image preprocessing

--- make_image_dataset.py
+++ make_image_dataset.py
@@ -9,11 +9,11 @@ def crop(input, height=128, width=128):
     im = Image.open(input)
     imgwidth, imgheight = im.size
     crops = []
-    for i in range(0,imgheight,height):
-        for j in range(0,imgwidth,width):
+    for i in range(0, imgheight-height, height):
+        for j in range(0, imgwidth-width, width):
             box = (j, i, j+width, i+height)
             c = im.crop(box)
-            c = np.asarray(c)
+            c = np.asarray(c, dtype=float)
             crops.append(c)
     return crops
 
@@ -22,5 +22,8 @@ for file_name in glob.glob(data_glob):
 	print('Croping file: %s' % (file_name))
 	crops += crop(file_name)
 data = np.array(crops)
+print('Data min: %d\nData max: %d' % (data.min(), data.max()))
+data -= data.min() # normalize between 0 and 1
+data /= data.max()
 print('Data shape: %s' % (str(data.shape)))
 np.save(out_file_name, data)
--- view_data.py
+++ view_data.py
@@ -3,8 +3,11 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
 data = np.load(sys.argv[1])
 for array in data:
-	plt.imshow(array)
+	plt.imshow(array, cmap='gray', vmin=0, vmax=1)
+	plt.colorbar()
 	plt.draw()
 	plt.pause(0.1)
+	plt.clf()
